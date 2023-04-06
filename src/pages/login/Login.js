507d4ddd91e795be0c6c4d301d956f2b3541302b
Farooq Marwat <mfarooq9a@gmail.com>
2023-04-06 12:58:23 +0500

    Login UI

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,12 +1,43 @@
 import React from 'react'
-import Signup from '../Signup/Signup'
+import './Login.css';
+import Button from '../../components/Button/Button';
 
-const Home = () => {
-  return (
-    <>
-        <Signup />
-    </>
-  )
+function Login() {
+  return (  
+
+      <>
+      <div class="container ">
+    <div class="pt-5">
+       
+                <div class="row">
+                    <div class="col-md-5 mx-auto">
+                        <div class="card card-body rounded">
+                            <h1 class="text-center">Login Page </h1>    
+                            <form id="submitForm" action="/login" method="post" data-parsley-validate="" data-parsley-errors-messages-disabled="true" novalidate="" _lpchecked="1"><input type="hidden" name="_csrf" value="7635eb83-1f95-4b32-8788-abec2724a9a4"/>
+                                
+                                
+                                <div class="form-group required">
+                                    <input type="text" placeholder="Email" class="form-control" id="mail" required="" name="mail" value=""/>
+                                </div>                    
+                                <div class="form-group required">
+                                    
+                                    <input type="password" placeholder="Password" class="form-control" required="" id="password" name="password" value=""/>
+                                </div>
+                                <div class="form-group pt-1">
+                                    <button class="btn btn-primary btn-block" type="submit">Log In</button>
+                                </div>
+                            </form>
+                            <p class="small-xl pt-3 text-center">
+                                <span class="text-muted">Not a member?</span>
+                                <a href="/signup"><b>Sign up</b></a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</div>
+      </>
+  );
 }
 
-export default Home
+export default Login
