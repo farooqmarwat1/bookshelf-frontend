05fbe1bcbfc748897a7037539f56a63665af362a
usmanmq00 <usmanmq00@gmail.com>
2023-04-12 20:18:14 +0500

    Rating Component Complete

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import book1 from '../images/book1.png';
+import Rating from '../components/Rating';
 
 const Book = ({ book }) => {
     return(
         <>
-            <Card className='my-3 p-3 rounded' >
-                <Link to={`/product/${product._id}`} >
-                    <Card.Img src={product.image} variant='top' />
+            <Card className='my-3 p-3 rounded card' >
+                <Link to={`/book/${book._id}`} >
+                    <Card.Img src={book1} style={{width:'100%',height: '250px'}} variant='top' />
                 </Link>
 
                 <Card.Body>
-                    <Link to={`/product/${product._id}`}>
+                    <Link to={`/book/${book._id}`}>
                         <Card.Title as='div' >
-                            <strong>{product.name}</strong>
+                            <strong>Title: {book.title}</strong>
                         </Card.Title>
                     </Link>
 
+                    <Card.Text as='div' >Author: {book.author}</Card.Text>
+
+                    <Card.Text as='div' >Access: {book.access}</Card.Text>
+
                     <Card.Text as='div' >
-                        {/* <Rating value={product.rating} text={ `${product.numReviews} reviews`} /> */}
-                        Rating
+                    <Rating value={book.rating} />
                     </Card.Text>
-                    <Card.Text>${ product.price }</Card.Text>
                 </Card.Body>
             </Card> 
         </>
