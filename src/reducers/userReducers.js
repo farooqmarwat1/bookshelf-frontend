f14ec9b7b31a2cc646bbe7594d1a0c70cff716ff
usmanmq00 <usmanmq00@gmail.com>
2023-04-14 00:49:12 +0500

    Redux added in Navbar and Logout

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -1,4 +1,5 @@
 import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT } from '../constants/usersConstant';
+import { USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL } from '../constants/usersConstant';
 
 export const userLoginReducer = (state = {  }, action) => {     // InitialState
     switch(action.type){   // Evaluate type in action object
@@ -13,4 +14,17 @@ export const userLoginReducer = (state = {  }, action) => {     // InitialState
         default:
             return state;
     }
+}
+
+export const userRegisterReducer = (state = {  }, action) => {     // InitialState
+    switch(action.type){   // Evaluate type in action object
+        case USER_REGISTER_REQUEST:
+            return { loading: true };      // Not Filled Yet
+        case USER_REGISTER_SUCCESS:
+            return { loading: false, userInfo: action.payload };
+        case USER_REGISTER_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    } 
 }
