2debfd4e1319d3fbb7d10ae4ce58b5109fdd5abe
usmanmq00 <usmanmq00@gmail.com>
2023-04-13 23:47:08 +0500

    Login API Integrated

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL } from '../constants/usersConstant';
 
-export const loginUser = () => async (dispatch) => {
+export const loginUser = (email, password) => async (dispatch) => {
     try{
         dispatch({ type: USER_LOGIN_REQUEST });
 
@@ -11,7 +11,7 @@ export const loginUser = () => async (dispatch) => {
             }
         };
         
-        const { data } = await axios.post('http://localhost:5000/api/users/login', { email, password }, config); 
+        const { data } = await axios.post('http://localhost:5000/api/users/login', { email: email, password: password }, config); 
 
         dispatch({ 
             type: USER_LOGIN_SUCCESS,
